f00f4ae0f8c664736203d8f0e4e66474c578408a
Almas Akchabayev <almas.akchabayev@gmail.com>
2021-12-28 20:12:01 +0200

    initial commit

--- bin/code-remote-ec2.ts
+++ bin/code-remote-ec2.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+require('dotenv').config();
+
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CodeRemoteEc2Stack } from '../lib/code-remote-ec2-stack';
@@ -11,11 +14,11 @@ new CodeRemoteEc2Stack(app, 'CodeRemoteEc2Stack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  // env: { account: '747058256098', region: 'eu-central-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/code-remote-ec2-stack.ts
+++ lib/code-remote-ec2-stack.ts
@@ -1,16 +1,83 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as constructs from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-export class CodeRemoteEc2Stack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CodeRemoteEc2Stack extends cdk.Stack {
+  constructor(scope: constructs.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      maxAzs: 1,
+      cidr: "10.0.0.0/21",
+      subnetConfiguration: [
+        {
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: "subnetPub",
+          cidrMask: 24,
+        },
+      ],
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CodeRemoteEc2Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const secGrp = new ec2.SecurityGroup(this, "CsSg", {
+      vpc: vpc,
+      securityGroupName: "csSg",
+      description: "Allow HTTP traffic to EC2 instance from anywhere",
+      allowAllOutbound: true,
+    });
+
+    secGrp.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.allTcp(), // Code-Server listen on 8080 port
+      "Allow all traffic"
+    );
+    // secGrp.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(8080), // Code-Server listen on 8080 port
+    //   "Allow ingress HTTP traffic"
+    // );
+    // secGrp.addIngressRule(
+    //   ec2.Peer.anyIpv4(),
+    //   ec2.Port.tcp(22),
+    //   "Allow ingress SSH traffic"
+    // );
+
+    // https://cloud-images.ubuntu.com/locator/ec2/
+    // owner: 099720109477 (ubuntu)
+    const imgLinuxUbu = new ec2.GenericLinuxImage({
+      "eu-central-1": "ami-0d527b8c289b4af7f", //  Ubuntu Server 20.04 LTS amd64
+    });
+
+    const instance = new ec2.Instance(this, "CsEc2Instance", {
+      vpc: vpc,
+      machineImage: imgLinuxUbu,
+      instanceName: "code-server-ec2",
+      instanceType: ec2.InstanceType.of(
+        ec2.InstanceClass.T2,
+        ec2.InstanceSize.MEDIUM
+      ),
+      securityGroup: secGrp,
+      blockDevices: [{
+        deviceName: '/dev/sda1', // Use the root device name from Step 1
+      volume: ec2.BlockDeviceVolume.ebs(64), // Override the volume size in Gibibytes (GiB)
+      }]
+    });
+
+    instance.addUserData(`#!/bin/sh
+
+cd /home/ubuntu
+sudo apt update
+sudo apt-get install build-essential
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+nvm install v14.17.0
+echo 'export NPM_TOKEN=${process.env.NPM_TOKEN}' >> ~/.bashrc
+npm install --global yarn
+echo 'custom initialization complete
+
+https://github.com/mazedesignhq/maze-monorepo.git
+git clone https://${process.env.GITHUB_USER}:${process.env.GITHUB_PERSONAL_ACCESS_TOKEN}@github.com/mazedesignhq/maze-monorepo.git
+cd maze-monorepo
+yarn
+`)
+    instance.instance.addPropertyOverride("KeyName", "almas-macbook-pro-2020");
   }
 }
